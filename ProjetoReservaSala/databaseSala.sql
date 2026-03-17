4623b8c77d0b318d1a9af3a233e4c20347e188d4
João Messias <jomefebarros@outlook.com>
2026-03-17 17:42:07 -0300

    adicionando o codeIgniter

---
+++ ProjetoReservaSala/databaseSala.sql
@@ -0,0 +1,68 @@
+#CRIAR A BASE DE DADOS
+create database mapa;
+
+#SELECIONAR O BANCO DE DADOS
+use mapa;
+
+#CRIANDO A TABELA DE USUARIO	
+create table tbl_usuario(
+	id_usuario integer not null auto_increment primary key,
+	nome varchar(50),
+	usuario varchar(15),
+	senha varchar(32),
+	email varchar(80),
+	dtcria datetime default now(),
+	estatus char(01) default ''
+);
+
+create table tbl_sala(
+	codigo integer primary key,
+	descricao varchar(30) default '',
+	andar integer,
+	capacidade integer,
+	dtcria datetime default now(),
+	estatus char(01 default ''
+);
+
+create table tbl_professor(
+	codigo integer auto_increment primary_key,
+	nome varchar(30) default '',
+	cpf varchar(11) default '',
+	tipo char(1) default 'F',
+	dtcria datetime default now(),
+	estatus char(01) default ''
+);
+
+create table tbl_turma(
+	codigo integer auto_increment primary key,
+	descricao varchar(50) default '',
+	hora_ini time,
+	hora_fim time,
+	dtcria datetime default now(),
+	estatus char(01) default 11
+);
+
+create table tbl_horario(
+	codigo integer auto_increment primary key,
+	descricao varchar(50) default '',
+	hora_ini time,
+	hora_fim time,
+	dtcria datetime default now(),
+	estatus char(01) default ''
+);
+
+create table tbl_mapa(
+	codigo 				integer auto_increment primary key,
+	datareserva 		date,
+	sala 				integer default 0,
+	codigo_horario 		integer default 0,
+	codigo_turma 		integer default 0,
+	codigo_professor 	integer default 0,
+	estatus char(01) default '',
+	
+	foreign key (sala) references tbl_sala(codigo),
+	foreign key (codigo_horario) references tbl_horario(codigo),
+	foreign key (codigo_turma) references tbl_turma(codigo),
+	foreign key (codigo_professor references tbl_professor(codigo)
+);
+
