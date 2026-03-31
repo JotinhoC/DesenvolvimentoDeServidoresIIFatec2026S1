7b6244f6b0921146e4777bede08ddc5164546428
João Messias <jomefebarros@outlook.com>
2026-03-31 20:27:28 -0300

    Add files via upload

--- ProjetoReservaSala/application/helpers/geral_helper.php
+++ ProjetoReservaSala/application/helpers/geral_helper.php
@@ -74,5 +74,50 @@ defined('BASEPATH') or exit('No direct script access allowed');
         //Valor default da variável $retorno caso não ocorra erro
         return array('codigoHelper' => 0, 'msg' => 'Validação correta.');
     }
+
+    //====================
+    //Função para verificar os tipos de dados para consulta
+    //====================
+
+    function validarDadosConsulta($valor, $tipo){
+        if ($valor != ''){
+            switch ($tipo){
+                case 'int':
+                    //filtra como inteiro, aceita '123' ou 123
+                    if (filter_var($valor, FILTER_VALIDATE_INT) === false){
+                        return array('codigoHelper' => 4, 'msg' => 'Conteúdo não inteiro.');
+                    }
+                    break;
+                case 'string':
+                    //Garante que é um string não vazia após trim
+                    if (!is_string($valor) || trim($valor) === ''){
+                        return array('codigoHelper' => 5, 'msg' => 'Conteúdo não é um texto');
+                    }
+                    break;
+                case 'date':
+                    //verifica se tem padrão de data
+                    if (!preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $valor, $match)){
+                        return array('codigoHelper' => 6, 'msg' => 'Data em formato inválido.');
+                    }else{
+                        //tenta criar DataTime no formato Y-m-d
+                        $d = DateTime::createFromFormat('Y-m-d', $valor);
+                        if(($d->format('Y-m-d') === $valor) == false){
+                            return array('codigoHelper' => 6, 'msg' => 'Data inválida');
+                        }
+                    }
+                    break;
+                case 'hora':
+                    //Verifico se tem padrão de hora
+                    if(!preg_match('/^(?:[01]\d|2[0-3]):[0-5]\d$/', $valor)){
+                        return array('codigoHelper' => 7, 'msg' => 'Hora em formato inválido.');
+                    }
+                    break;
+                default:
+                    return array('codigoHelper' => 97, 'msg' => 'Tipo de dado não definido.');
+            }
+        }
+        //Valor default da variável $retorno caso não ocorra erro
+        return array('codigoHelper' => 0, 'msg' => 'Validação correta');
+    }
 ?>
 
