964d4302162d5f19af4346302b77243ccbf8e9f1
João Messias <jomefebarros@outlook.com>
2026-05-22 23:37:18 -0300

    Add files via upload

--- ProjetoReservaSala/databaseSala.sql
+++ ProjetoReservaSala/databaseSala.sql
@@ -66,3 +66,14 @@ create table tbl_mapa(
 	foreign key (codigo_professor) references tbl_professor(codigo)
 );
 
+#CRIAR TABELA DE USUÁRIOS
+
+create table tbl_usuario(
+	id_usuario integer auto_increment primary key,
+	nome varchar(30),
+	email varchar(40),
+	usuario varchar(20),
+	senha char(32),
+	dtcria datetime default now(),
+	estatus char(01) default ''
+);
